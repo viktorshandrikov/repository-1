54dbac00e5c932b464440d27c070ea5a30de5938
Виктор Шандриков <114388157+viktorblaming@users.noreply.github.com>
2022-11-12 10:11:15 +0700

    Update КодированиеN.py

--- КодированиеN.py
+++ КодированиеN.py
@@ -4,22 +4,20 @@ def i(I, K):
     return I/K
 def I(K, i):
     return K*i
-print('Посчитать Объём сообщения=1')
-print('Посчитать объём символа=2')
-print('Посчитать количество символов=3')
-q=int(input())
-if q==1:
-    i=float(input('Объём одного символа'))
-    K=float(input('Количество символов'))
-    print(I(K, i))
-if q==2:
-    I=float(input('Объём сообщения'))
-    K=float(input('Количество символов'))
-    print(i(I, K))
-if q==3:
-    I=float(input('Объём сообщения'))
-    i=float(input('Количество символов'))
-    print(K(I, i))
-
-
-
+if p==1:
+    print('Посчитать Объём сообщения=1')
+    print('Посчитать объём символа=2')
+    print('Посчитать количество символов=3')
+    q=int(input())
+    if q==1:
+        i=float(input('Объём одного символа'))
+        K=float(input('Количество символов'))
+        print(I(K, i))
+    if q==2:
+        I=float(input('Объём сообщения'))
+        K=float(input('Количество символов'))
+        print(i(I, K))
+    if q==3:
+        I=float(input('Объём сообщения'))
+        i=float(input('Количество символов'))
+        print(K(I, i))
